312dc6f5c0b94f6d590af896f6e987b509888f5f
Julien Lemaire <jujulem06@hotmail.fr>
2016-05-04 16:23:55 +0200

    Added commentaries

--- Miaou.java
+++ Miaou.java
@@ -5,7 +5,7 @@ import java.util.stream.*;
 public class Miaou {
 	static ArrayList<Predicate<ArrayList<String>>> filterList;
 
-
+	//Filters the parsed CSV as a Stream and returns another Stream filtered with filters in filterList. (We like filters).
 	public static Stream<ArrayList<String>> miaouFilter(Stream<ArrayList<String>> s) {
 		Stream<ArrayList<String>> t = s;
 		for(Predicate<ArrayList<String>> lambda: filterList) {
@@ -14,24 +14,27 @@ public class Miaou {
 		return t;
 	}
 
+	//Filter a Stream (with a specified Predicate lambda) and returns it
 	public static Stream<ArrayList<String>> runFilter(Stream<ArrayList<String>> s, Predicate<ArrayList<String>> lambda) {
 		return s.filter(lambda);
 	}
 
+	//Add a filter (as a lambda) in FilterList
 	public static void addFilter(Predicate<ArrayList<String>> lambda) {
 		filterList.add(lambda);
 	}
 
 	public static void main(String[] args) {
-		ReadCSV tt = new ReadCSV();
+		ReadCSV tt = new ReadCSV(); //CSV file is parsed
 		tt.printColumns();
 
 		filterList = new ArrayList<Predicate<ArrayList<String>>>();
-		addFilter(b -> b.get(2).contains("Toulon"));
-		addFilter(b -> !b.get(13).isEmpty());
+		addFilter(b -> b.get(2).contains("Toulon")); //add a filter for Toulon universities
+		addFilter(b -> !b.get(13).isEmpty()); //add a filter for those which answered the poll (with an answer rate)
 
-		Stream<ArrayList<String>> s = tt.getData().stream();
+		Stream<ArrayList<String>> s = tt.getData().stream(); //Convert the parsed CSV as a stream
 //		System.out.println(miaouFilter(s).map);
+		//Returns the average answer rate in Toulon
 		System.out.println( miaouFilter(s).mapToInt(b -> Integer.parseInt(b.get(12))).average().orElse(0) );
 
 
