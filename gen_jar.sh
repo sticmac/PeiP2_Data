06aeffa29aeae22fa3270e960bf7bc31cf0f8f8b
piernov <piernov@piernov.org>
2016-05-23 08:26:40 +0200

    Update gen_jar.sh - exclude git

--- gen_jar.sh
+++ gen_jar.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-jar cvfe lemaire-novac-2.jar sticmacpiernov.spreadsheet.Spreadsheet .
+jar cvfe lemaire-novac-2.jar sticmacpiernov.spreadsheet.Spreadsheet `find . -not -path "*/.git/*" -not -type d -not -path "*/.gitignore"`
